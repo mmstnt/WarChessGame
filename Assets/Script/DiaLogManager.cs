4813c258797322e4dd93f078a55799022e9b98a6
mmstnt <133971609+mmstnt@users.noreply.github.com>
2025-06-26 00:12:28 +0800

    2025/06/26 第2次上傳
    1.裝了物件欄顏色輔助插件
    2.基礎對話UI介面

--- Assets/Script/DiaLogManager.cs
+++ Assets/Script/DiaLogManager.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DiaLogManager : MonoBehaviour
 {
-    
+    [Header("文本")]
+    public TextAsset dialogDataFile;
+    [Header("圖像")]
+    public Sprite sprite;
+    [Header("文字物件")]
+    public TMP_Text nameText;
+    public TMP_Text dialogText;
 }
